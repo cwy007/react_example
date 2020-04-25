fe031b9f970a9ae70eb0818d53175a483823b550
chenweiyan <chanweiyan007@gmail.com>
2020-04-25 14:00:54 +0800

    组合 vs 继承

--- src/index.js
+++ src/index.js
@@ -1,102 +1,109 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './split_panel.css';
 import * as serviceWorker from './serviceWorker';
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
 }
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-    this.state = { temperature: '', scale: 'c' };
-  }
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
 
-  handleCelsiusChange(temperature) {
-    this.setState({ scale: 'c', temperature });
-  }
+function Contacts() {
+  return <div className="Contacts" />;
+}
 
-  handleFahrenheitChange(temperature) {
-    this.setState({ scale: 'f', temperature });
-  }
+function Chat() {
+  return <div className="Chat" />;
+}
 
-  render() {
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
-    return (
-      <div>
-        <TemperatureInput
-          scale="c"
-          temperature={celsius}
-          onTemperatureChange={this.handleCelsiusChange} />
-        <TemperatureInput
-          scale="f"
-          temperature={fahrenheit}
-          onTemperatureChange={this.handleFahrenheitChange} />
-        <BoilingVerdict
-          celsius={parseFloat(celsius)} />
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
       </div>
-    );
-  }
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
 }
 
-const scaleNames = {
-  c: 'Celsius',
-  f: 'Fahrenheit'
-};
+function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
 
-class TemperatureInput extends React.Component {
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </FancyBorder>
+  );
+}
+
+class SignUpDialog extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = { login: '' };
   }
 
   render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
     return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature}
+      <Dialog title="Mars Exploration Program"
+        message="How should we refer to you?">
+        <input value={this.state.login}
           onChange={this.handleChange} />
-      </fieldset>
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
     );
   }
-}
 
-function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
-}
-
-function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
+  handleChange(e) {
+    this.setState({ login: e.target.value });
   }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
 }
 
 ReactDOM.render(
-  <Calculator />,
+  <SignUpDialog />,
   document.getElementById('root')
 );
 
